f3788980ad033e900c940901033694dd29c8e5fe
SashaShch <such.shch93@gmail.com>
2020-08-15 11:05:36 +0300

    Initial Commit

---
+++ testTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testTask
+//
+//  Created by Рома on 15.08.2020.
+//  Copyright © 2020 SashaShch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
